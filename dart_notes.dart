4397257acc38ec89c77723fd20d51e203e8c7175
bubjoin <bubjoin@gmail.com>
2023-11-09 04:36:32 +0900

    Add dart_notes.dart main var

---
+++ dart_notes.dart
@@ -0,0 +1,42 @@
+// File names
+// dart_notes.dart
+// lowercase for no case-sensitive system
+// underscore(_) for readability
+
+/* Top-level main() function
+void main() {
+  print('Hello, Dart!');
+}
+
+int main() {
+  print('Hello, Dart!');
+  return 0;
+}
+*/
+
+/* Variables
+// var
+void main() {
+  var name = 'Dart'; // var, type inference, determined by the initial value
+  // name = 1; // error, after the variable type determined as String
+  var year = 2011;
+  var marketShare = 0.1; // to grow
+  var features = ['OOP', 'cross-platform', 'type-safe', 'AOT', 'JIT', 'async'];
+  var flutter = {
+    'platform': [
+      'Android',
+      'Linux',
+      'Mac',
+      'Windows',
+    ],
+    'language': 'Dart', // notice this comma makes diffrence in editor
+  };
+  print('${name}');
+  print('${year}');
+  print('${marketShare}');
+  print('${features}');
+  print('${flutter}');
+}
+
+*/
+
